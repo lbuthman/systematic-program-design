3f375824bab4b049869c9104f7de2e40ecf2a92d
lbuthman <lbuthman@gmail.com>
2017-05-07 13:13:11 -0500

    add list example

---
+++ Week6/list-example.rkt
@@ -0,0 +1,25 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname list-example) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+empty
+
+(define L1 (cons "Flames" empty))
+(define L2 (cons "Leaves" (cons "Flames" empty)))
+(define L3 (cons (square 10 "solid" "blue")
+                 (cons (triangle 20 "solid" "green")
+                       empty)))
+
+(first L1)
+(first L2)
+(first L3)
+
+(rest L1)
+(rest L2)
+(rest L3)
+
+(first (rest L2))
+
+(empty? empty)
+(empty? L1)
